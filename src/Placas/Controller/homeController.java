19cd8281c56aedf9b5b035d9518fb946d076620c
LuisEduardoLopez33 <193274@ids.upchiapas.edu.mx>
2022-01-31 20:38:11 -0600

    estructura

---
+++ src/Placas/Controller/homeController.java
@@ -0,0 +1,23 @@
+package Placas.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class homeController {
+
+    @FXML
+    private TextField entradaUno;
+
+    @FXML
+    private TextField entradaDos;
+
+    @FXML
+    private TextField entradaTres;
+
+    @FXML
+    void butttonInicio(MouseEvent event) {
+
+    }
+
+}
