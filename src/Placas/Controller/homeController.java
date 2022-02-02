71a15cabf5f3d270a3d2deb588c10c60c6192d3c
LuisEduardoLopez33 <193274@ids.upchiapas.edu.mx>
2022-02-02 12:44:12 -0600

    FINAl ici

--- src/Placas/Controller/homeController.java
+++ src/Placas/Controller/homeController.java
@@ -1,12 +1,19 @@
 package Placas.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class homeController {
+public class homeController implements Initializable {
 
     private String cadena;
     private String[] dicionario = {"A","B","C","D","E","F","G","H","J","K","L","M","N","P","R","S","T","U","V","W","X","Y","Z"};
@@ -25,6 +32,28 @@ public class homeController {
     private TextField entradaTres;
 
     @FXML
+    private Label estadoLabel;
+
+    @FXML
+    private Label tipoLabel;
+
+    @FXML
+    private Label errorMensaje;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Label edoMex;
+
+    @FXML
+    private Label sanLuis;
+
+    @FXML
+    private ImageView imgEscudo;
+
+
+    @FXML
     void butttonInicio(MouseEvent event) {
 
 
@@ -34,12 +63,12 @@ public class homeController {
         for (int i=0; i<cadena.length(); i++){
             list.add(cadena.substring(i,i+1));
         }
-//        System.out.println("Imprimiendo Array...");
-//        for (int l=0; l< list.size(); l++){
-//            System.out.println(list.get(l));
-//        }
-
-
+        sanLuis.setVisible(false);
+        edoMex.setVisible(false);
+        errorMensaje.setVisible(false);
+        errorLabel.setText(" ");
+        estadoLabel.setText(" ");
+        tipoLabel.setText(" ");
 
         switch (list.get(0)){
             case "K":
@@ -74,6 +103,8 @@ public class homeController {
                 break;
             default:
                 System.out.println("No esta dentro de Nuestro Rango");
+                errorMensaje.setVisible(true);
+                errorLabel.setText("No esta dentro de Nuestro Rango");
                 list.clear();
         }
 
@@ -81,43 +112,61 @@ public class homeController {
 
     public void q1(int zice, int limit){
         String sms = "no esta dentro de nuestro rango";
+        boolean error= true;
         //System.out.println("entro a q1  -->"+dicionario[6]);
         for (int i=zice; i<limit; i++){
             if (list.get(1).equals(dicionario[i])){
                 System.out.println("Pasando a q2");
+                error = false;
                 q2();
                 i=limit;
-                sms="";
-            }else {
-
             }
         }
+        if(error){
+            System.out.println("No esta dentro de Nuestro Rango");
+            errorMensaje.setVisible(true);
+            errorLabel.setText("No esta dentro de Nuestro Rango");
+            list.clear();
+        }
 
-        System.out.println(sms);
         list.clear();
     }
 
     public void q2(){
-
+        boolean error= true;
         for (int i=0; i<dicionario.length; i++){
             if (list.get(2).equals(dicionario[i])){
                 System.out.println("Pasando a q3");
+                error= false;
                 q3();
                 i= dicionario.length;
             }
         }
 
         if (list.get(2).equals("-")){
+            error = false;
             q10();
+        }
+        if(error){
+            System.out.println("No esta dentro de Nuestro Rango");
+            errorMensaje.setVisible(true);
+            errorLabel.setText("No esta dentro de Nuestro Rango");
         }
 
     }
 
     public void q3(){
+        boolean error= true;
         tipo = "AUTOMOVIL";
         if (list.get(3).equals("-")){
+            error= false;
             System.out.println("PASANDO A 4");
             q4q5q6();
+        }
+        if(error){
+            System.out.println("No esta dentro de Nuestro Rango");
+            errorMensaje.setVisible(true);
+            errorLabel.setText("No esta dentro de Nuestro Rango");
         }
     }
 
@@ -141,25 +190,43 @@ public class homeController {
         if (status && error){
             System.out.println("pasando a 7");
             q7();
+        }else{
+            System.out.println("No esta dentro de Nuestro Rango");
+            errorMensaje.setVisible(true);
+            errorLabel.setText("No esta dentro de Nuestro Rango");
         }
 
     }
 
     public void q7(){
+        boolean error= true;
         if (list.get(7).equals("-")){
             System.out.println("pasando a 8");
+            error= false;
             q8();
+        }
+        if(error){
+            System.out.println("No esta dentro de Nuestro Rango");
+            errorMensaje.setVisible(true);
+            errorLabel.setText("No esta dentro de Nuestro Rango");
         }
         //System.out.println("llegue a q7 xd xd xd");
     }
 
     public void q8(){
+        boolean error= true;
         for (int i=0; i<dicionario.length; i++){
             if (list.get(8).equals(dicionario[i])){
                 System.out.println("pasando a 9");
+                error= false;
                 q9Aceptacion();
                 i= dicionario.length;
             }
+        }
+        if(error){
+            System.out.println("No esta dentro de Nuestro Rango");
+            errorMensaje.setVisible(true);
+            errorLabel.setText("No esta dentro de Nuestro Rango");
         }
     }
 
@@ -168,6 +235,7 @@ public class homeController {
         //System.out.println("Aceptaciion");
         if (tipo.equals("CAMION")){
             if (list.get(0).equals("M")){
+
                 aceptacion = valid(0, 16);
             }
         }
@@ -181,6 +249,18 @@ public class homeController {
         if (aceptacion){
             System.out.println("Estado: "+estado);
             System.out.println("Tipo de Veiculo: "+tipo);
+            estadoLabel.setText(estado);
+            tipoLabel.setText(tipo);
+            if(estado.equals("edoMex")){
+                edoMex.setVisible(true);
+            }else{
+                sanLuis.setVisible(true);
+            }
+        }else{
+            System.out.println("No esta dentro de Nuestro Rango");
+            errorMensaje.setVisible(true);
+            errorLabel.setText("No esta dentro de Nuestro Rango");
+
         }
     }
 
@@ -196,15 +276,26 @@ public class homeController {
     }
 
     public void q10(){
+        boolean error = true;
         tipo = "CAMION";
         try {
             int pos = Integer.parseInt(list.get(3));
             if (pos>=0 && pos<=9){
+                error= false;
                q4q5q6();
             }
         }catch (Exception e){
             System.out.println("No se Acptan letras en esta area xd");
         }
+        if(error){
+            System.out.println("No esta dentro de Nuestro Rango");
+            errorMensaje.setVisible(true);
+            errorLabel.setText("No esta dentro de Nuestro Rango");
+        }
+    }
+    @FXML
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        imgEscudo.setImage(new Image("Recursos/ESCUDO4.png"));
     }
 
 }
